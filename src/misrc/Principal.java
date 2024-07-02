5de8f0383f5a2a9dc3e73a56c138b94e66422d56
mathewprogram <ionitza.matei@icloud.com>
2024-07-02 10:39:59 +0200

    Version 1.3

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -6,8 +6,7 @@ public class Principal {
         System.out.println("Hola GitHub");
         System.out.println("Hola Commit");
         System.out.println("Hola Clone");
-        //este es un comentario en el original
-        //este es un comentarion en la clona
+       
     }
     
 }
---
+++ src/misrc/Principal1.java
@@ -0,0 +1,11 @@
+package misrc;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
