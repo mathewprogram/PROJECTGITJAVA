ffa8a4785574cf2a10e5e0ff6af404e872344018
mathewprogram <ionitza.matei@icloud.com>
2024-07-02 10:32:56 +0200

    Version 1.2

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola GitHub");
         System.out.println("Hola Commit");
+        System.out.println("Hola Clone");
+        //este es un comentario en el original
+        //este es un comentarion en la clona
     }
     
 }
